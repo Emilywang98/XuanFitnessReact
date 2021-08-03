dbf04941874159e704534af3f17bca059f010ca2
Emily Wang <ewang56@uwo.ca>
2021-08-02 20:13:17 -0600

    updated habits

--- paper-dashboard-react-main/src/views/Tables.js
+++ paper-dashboard-react-main/src/views/Tables.js
@@ -1,71 +1,244 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+/*
+Habits
 */
 
 import React, { useState } from "react";
+
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col,
+} from "reactstrap";
  
 function Tables() {
-  const [fields, setFields] = useState([{ value: null }]);
 
-  function handleChange(i, event) {
-    const values = [...fields];
-    values[i].value = event.target.value;
-    setFields(values);
-  }
-
-  function handleAdd() {
-    const values = [...fields];
-    values.push({ value: null });
-    setFields(values);
-  }
-
-  function handleRemove(i) {
-    const values = [...fields];
-    values.splice(i, 1);
-    setFields(values);
-  }
+  const [inputList, setInputList] = useState([{ habit: ""}]);
+ 
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputList];
+    list[index][name] = value;
+    setInputList(list);
+  };
+ 
+  // handle click event of the Remove button
+  const handleRemoveClick = index => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
+ 
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, { habit: "" }]);
+  };
 
   return (
-    <div className="App">
+    //this is the user info section on the top left
+    <>
+      <div className="content">
+        <Row>
+          <Col md="4">
+            <Card className="card-user">
+              <div className="image">
+                <img
+                  alt="..."
+                  src={require("assets/img/damir-bosnjak.jpg").default}
+                />
+              </div>
+              <CardBody>
+                <div className="author">
+                  <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                    <img
+                      alt="..."
+                      className="avatar border-gray"
+                      src={require("assets/img/mike.jpg").default}
+                    />
+                    <h5 className="title">Xuan-Nguyen-Marshall</h5>
+                  </a>
+                  <p className="description">@xuanfitness</p>
+                </div>
+                <p className="description text-center">
+                  "I love working out <br />
+                  and you should too! <br /> :D"
+                </p>
+              </CardBody>
+              {/* <CardFooter>
+                <hr />
+                <div className="button-container">
+                  <Row>
+                    <Col className="ml-auto" lg="3" md="6" xs="6">
+                      <h5>
+                        12 <br />
+                        <small>Files</small>
+                      </h5>
+                    </Col>
+                    <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                      <h5>
+                        2GB <br />
+                        <small>Used</small>
+                      </h5>
+                    </Col>
+                    <Col className="mr-auto" lg="3">
+                      <h5>
+                        24,6$ <br />
+                        <small>Spent</small>
+                      </h5>
+                    </Col>
+                  </Row>
+                </div>
+              </CardFooter> */}
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle tag="h4">Other members</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <ul className="list-unstyled team-members">
+                  <li>
+                    <Row>
+                      <Col md="2" xs="2">
+                        <div className="avatar">
+                          <img
+                            alt="..."
+                            className="img-circle img-no-padding img-responsive"
+                            src={
+                              require("assets/img/faces/ayo-ogunseinde-2.jpg")
+                                .default
+                            }
+                          />
+                        </div>
+                      </Col>
+                      <Col md="7" xs="7">
+                        DJ Khaled <br />
+                        <span className="text-muted">
+                          {/* <small>Offline</small> */}
+                        </span>
+                      </Col>
+                      <Col className="text-right" md="3" xs="3">
+                        <Button
+                          className="btn-round btn-icon"
+                          color="success"
+                          outline
+                          size="sm"
+                        >
+                          <i className="fa fa-envelope" />
+                        </Button>
+                      </Col>
+                    </Row>
+                  </li>
+                  <li>
+                    <Row>
+                      <Col md="2" xs="2">
+                        <div className="avatar">
+                          <img
+                            alt="..."
+                            className="img-circle img-no-padding img-responsive"
+                            src={
+                              require("assets/img/faces/joe-gardner-2.jpg")
+                                .default
+                            }
+                          />
+                        </div>
+                      </Col>
+                      <Col md="7" xs="7">
+                        Emily Wang<br />
+                        <span className="text-success">
+                          {/* <small>Available</small> */}
+                        </span>
+                      </Col>
+                      <Col className="text-right" md="3" xs="3">
+                        <Button
+                          className="btn-round btn-icon"
+                          color="success"
+                          outline
+                          size="sm"
+                        >
+                          <i className="fa fa-envelope" />
+                        </Button>
+                      </Col>
+                    </Row>
+                  </li>
+                  <li>
+                    <Row>
+                      <Col md="2" xs="2">
+                        <div className="avatar">
+                          <img
+                            alt="..."
+                            className="img-circle img-no-padding img-responsive"
+                            src={
+                              require("assets/img/faces/clem-onojeghuo-2.jpg")
+                                .default
+                            }
+                          />
+                        </div>
+                      </Col>
+                      <Col className="col-ms-7" xs="7">
+                        Vivian Wu<br />
+                        <span className="text-danger">
+                          {/* <small>Busy</small> */}
+                        </span>
+                      </Col>
+                      <Col className="text-right" md="3" xs="3">
+                        <Button
+                          className="btn-round btn-icon"
+                          color="success"
+                          outline
+                          size="sm"
+                        >
+                          <i className="fa fa-envelope" />
+                        </Button>
+                      </Col>
+                    </Row>
+                  </li>
+                </ul>
+              </CardBody>
+            </Card>
+          </Col>
 
-      <button type="button" onClick={() => handleAdd()}>
-        +
-      </button>
-
-      {fields.map((field, idx) => {
-        return (
-               
-          <div key={`${field}-${idx}`}>
-        <label>Enter a Question</label>
-        <input
-          type="text"
-          placeholder=""
-          onChange={e => handleChange(idx, e)}
-        />
-        <button type="button" onClick={() => handleRemove(idx)}>
-          X
-        </button>
-      </div> 
-        );
-        })}
-    </div>
-    
+          <Col md="8">
+            <Card className="card-user">
+              <CardHeader>
+                <CardTitle tag="h5">Add Habit</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <Form>
+                  <Row>
+                    <Col className="pr-1" md="5">
+                      <FormGroup>
+                        <label>Enter Question</label>
+                        <Input
+                          defaultValue=""
+                          // disabled
+                          placeholder="Question"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col> 
+                    {/* <div className="btn-box">
+                  {inputList.length !== 1 && <button
+                    className="mr10"
+                    onClick={() => handleRemoveClick(i)}>Remove</button>}
+                  {inputList.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
+            </div> */}
+                  </Row>
+                </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      </div>
+    </>
   );
+          
 }
  
 export default Tables;
